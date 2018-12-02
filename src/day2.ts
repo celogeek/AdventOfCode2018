5ce7f1c88c8451d5288710c8d651971041fe02bf
celogeek <me@celogeek.com>
2018-12-02 14:32:49 +0100

    optimize day2

--- src/day2.ts
+++ src/day2.ts
@@ -13,35 +13,50 @@ data.forEach((row) => {
         count.set(l, (count.get(l) || 0) + 1);
     }
     const result = new Set(count.values());
-    if (result.has(2)) {
-        match2++;
-    }
-    if (result.has(3)) {
-        match3++;
-    }
     if (result.has(2) || result.has(3)) {
         match.push(row);
+        if (result.has(2)) {
+            match2++;
+        }
+        if (result.has(3)) {
+            match3++;
+        }
     }
 });
 
 console.log("CHECKSUM:", match2, "*", match3, "=", match2 * match3);
-for (const a of match) {
-    for (const b of match) {
-        if (a === b) {
-            continue;
-        }
-        let d = 0;
-        let letters = "";
-        for (let i = 0; i < a.length; i++) {
-            if (a[i] !== b[i]) {
-                d++;
-            } else {
-                letters += a[i];
-            }
-        }
-        if (d === 1) {
-            console.log("Response", letters);
+
+// for (const a of match) {
+//     for (const b of match) {
+//         if (a === b) {
+//             continue;
+//         }
+//         let d = 0;
+//         let letters = "";
+//         for (let i = 0; i < a.length; i++) {
+//             if (a[i] !== b[i]) {
+//                 d++;
+//             } else {
+//                 letters += a[i];
+//             }
+//         }
+//         if (d === 1) {
+//             console.log(letters);
+//             // process.exit(0);
+//         }
+//     }
+// }
+
+const pairs = new Set<string>();
+for (const boxId of match) {
+    for (let i = 0; i < boxId.length; i++) {
+        const w = boxId.slice(0, i) + " " + boxId.slice(i + 1);
+        if (pairs.has(w)) {
+            // console.log(w);
+            console.log(w.replace(" ", ""));
             process.exit(0);
+        } else {
+            pairs.add(w);
         }
     }
 }
